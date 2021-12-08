f082375b20e3e19d29046fc794b234444b87f18f
perryn <shevchukmagnumpetro@gmail.com>
2021-12-08 10:53:53 +0200

    75a296e

---
+++ _nuxt/static/1638953622/ccpa/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/ccpa", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1638953622/privacy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmassive-ccp-us-deploy\u002F_nuxt\u002Fstatic\u002F1638953622",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprivacy",config:{_app:{basePath:"\u002Fmassive-ccp-us-deploy\u002F",assetsPath:"\u002Fmassive-ccp-us-deploy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
